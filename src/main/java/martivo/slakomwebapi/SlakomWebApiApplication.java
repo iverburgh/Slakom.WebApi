eebdf5b514610a2f072a4790a404ed0a3a3b3907
Ivo Verburgh <i.verburgh@4dotnet.nl>
2022-01-03 20:27:26 +0100

    Initial commit

---
+++ src/main/java/martivo/slakomwebapi/SlakomWebApiApplication.java
@@ -0,0 +1,13 @@
+package martivo.slakomwebapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlakomWebApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlakomWebApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/martivo/slakomwebapi/SlakomWebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package martivo.slakomwebapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlakomWebApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
